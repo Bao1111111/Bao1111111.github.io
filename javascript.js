a3c6c7c859595b19a1cc14d02dad9dc76277fb09
Bao <143947499+Bao1111111@users.noreply.github.com>
2023-12-15 14:01:25 +0100

    finish the Rock paper Scissor Game

--- javascript.js
+++ javascript.js
@@ -1,87 +1,45 @@
 
-const options = ["Rock", "Paper", "Scissor"]
+let playerScore = 0;
+let computerScore = 0;
 
-function getComputerChoice(){
-    const choice = options[Math.floor(Math.random()*options.length)]
-    return choice
+function computerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex];
 }
 
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    gameWinner = 'tie'
-  }
-  if (
-    (playerSelection === 'Rock' && computerSelection === 'Scissor') ||
-    (playerSelection === 'Scissor' && computerSelection === 'Paper') ||
-    (playerSelection === 'Paper' && computerSelection === 'Rock')
-  ) {
-    playerWin++
-    gameWinner = 'player'
-  }
-  if (
-    (computerSelection === 'Rock' && playerSelection === 'Scissor') ||
-    (computerSelection === 'Scissor' && playerSelection === 'Paper') ||
-    (computerSelection === 'Paper' && playerSelection === 'Rock')
-  ) {
-    computerWin++
-    gameWinner = 'computer'
-  }
-
-
-  let playerWin = 0;
-  let computerWin = 0;
-  let gameWinner = ""
-
-function game(){
-
-// the button selection
-const buttons = document.querySelectorAll("button")
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-   playerSelection = button.className;
-   const computerSelection = getComputerChoice();
-   battleWinText.textContent = (playRound(playerSelection, computerSelection))
-   playerWinText.textContent = "The Players total Wins : " + playerWin;
-   computerWinText.textContent = "The Computers total Wins : " + computerWin
-   gameEnd()
-  })
-})
-
-// Div Container for every Result
-const container = document.querySelector("#container");
-const resultDiv = document.createElement("div");
-resultDiv.style.marginTop = "40px";
-container.appendChild(resultDiv)
-
-
-// tracks the Wins of the Player
-const playerWinText = document.createElement("p");
-playerWinText.style.color = "blue"
-playerWinText.textContent = "You have Won : " + playerWin
-resultDiv.appendChild(playerWinText);
-
-// tracks the Wins of the Computer
-const computerWinText = document.createElement("p")
-computerWinText.style.color = "red"
-computerWinText.textContent = "The Computer has Won : " + computerWin
-resultDiv.appendChild(computerWinText)
-
-// battles won
-const battleWinText = document.createElement("p")
-battleWinText.style.color = "green"
-resultDiv.appendChild(battleWinText)
-
-// Who won the Game
-const gameWinText = document.createElement("p")
-gameWinText.style.color = "gold"
-gameWinText.textContent = gameWinner
-resultDiv.appendChild(gameWinText)
-
-function gameEnd(){
-  if (playerWin == 5){
-    gameWinner = "You Win!"
-    gameWinText.textContent = gameWinner
-  }
+function playRound(playerSelection) {
+    const computerSelection = computerChoice();
+    const result = determineWinner(playerSelection, computerSelection);
+    updateScore(result);
+    displayResult(result, playerSelection, computerSelection);
 }
 
+function determineWinner(player, computer) {
+    if (player === computer) {
+        return "It's a tie!";
+    } else if (
+        (player === "rock" && computer === "scissors") ||
+        (player === "paper" && computer === "rock") ||
+        (player === "scissors" && computer === "paper")
+    ) {
+        playerScore++;
+        return "You win!";
+    } else {
+        computerScore++;
+        return "Computer wins!";
+    }
+}
+
+function updateScore(result) {
+    const scoreElement = document.getElementById("score");
+    scoreElement.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
+    if (playerScore === 5) {
+        scoreElement.textContent = "You have Won!"
+    }else if(computerScore === 5) { scoreElement.textContent = "The Computer has Won!"}
+}
+
+function displayResult(result, player, computer) {
+    const resultElement = document.getElementById("result");
+    resultElement.textContent = `${result} You chose ${player}, computer chose ${computer}.`;
 }
